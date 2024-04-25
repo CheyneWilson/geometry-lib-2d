5c3bf2e8270496487f84d6354b9f355057b8cfcb
Cheyne Wilson <dev@cheyne.nz>
2024-04-25 21:59:42 +1000

    Add functions to Vector2 prototype

--- src/point.ts
+++ src/point.ts
@@ -1,6 +1,158 @@
-type Vector2 = {
-    x: number,
-    y: number
+type TVector2 = {
+    x: number;
+    y: number;
+}
+
+class Vector2 {
+    x: number;
+    y: number;
+
+    constructor(x: number, y: number) {
+        this.x = x;
+        this.y = y;
+    }
+
+    /**
+     * Subtract another Vector2
+     *
+     * @param other A Vector2-like object
+     */
+    public subtract(other: TVector2): Vector2 {
+        this.x -= other.x;
+        this.y -= other.y;
+        return this
+    }
+
+    /**
+     * Return the (counter-clockwise) angle between this vector and the x-axis (in radians)
+     *
+     *       . a
+     *      /
+     *     /
+     *    /
+     *   O-------> x
+     *
+     * E.g. for point {x: 1, y: 1}, the angle is PI / 4 radians
+     */
+    public angle(): number {
+        return Math.atan2(-this.y, -this.x) + Math.PI;
+    }
+
+
+    /**
+     * Calculate the length-squared of this vector
+     *
+     * This is a convenience function that can be used to optimize some calculations
+     * because it is cheaper to calculate than the length.
+     *
+     */
+    public lengthSquared(): number {
+        return this.x * this.x + this.y * this.y;
+    }
+
+    /**
+     * Calculate the length of this vector
+     *
+     */
+    public length(): number {
+        return Math.sqrt(this.lengthSquared())
+    }
+
+    /**
+     * The dot product of two vectors
+     *
+     * @param a A vector
+     * @param b Another vector
+     */
+    public static dot(a: TVector2, b: TVector2): number {
+        return a.x * b.x + a.y * b.y;
+    }
+
+    /**
+     * The determinant of two vectors
+     *
+     * @param a A vector
+     * @param b Another vector
+     */
+    public static det(a: TVector2, b: TVector2): number {
+        return a.x * b.y - b.x * a.y;
+    }
+
+    /**
+     * Calculate the counter-clockwise angle (in radians) between two vectors.
+     *
+     * @param other Another vector
+     */
+    public angleBetween(other: TVector2): number {
+        // If two points are identical, the angle between them is not defined
+        if (this === other) {
+            return NaN;
+        }
+
+        let otherLengthSquared = Vector2.from(other).lengthSquared()
+        const denominator = Math.sqrt(this.lengthSquared() * otherLengthSquared);
+        let c = Vector2.dot(this, other) / denominator;
+
+        // Handle possible numerical problems
+        let theta = Math.acos(clamp(c, -1, 1));
+
+        const rotation = Math.sign(Vector2.det(this, other))
+        if (rotation === -1) {
+            theta = Math.PI * 2 - theta
+        }
+
+        return theta
+    }
+
+    /**
+     * Calculate the distance between this point and another
+     *
+     * @param other Another point
+     */
+    public distance(other: TVector2): number {
+        return Math.sqrt(this.distanceSquared(other));
+    }
+
+    /**
+     * Calculate the distance-squared between this point and another
+     *
+     * This is a convenience public that can be used to optimize some calculations
+     * because it is cheaper to calculate than the distance.
+     *
+     * @param other Another point
+     */
+    public distanceSquared(other: TVector2) {
+        const x = this.x - other.x;
+        const y = this.y - other.y;
+        return x * x + y * y;
+    }
+
+    /**
+     * Add another vector to this
+     *
+     * @param other A vector
+     */
+    public add(other: TVector2): Vector2 {
+        this.x += other.x;
+        this.y += other.y;
+        return this
+    }
+
+    /**
+     * Create a new 2d point/vector from polar coordinates
+     *
+     * @param length The length of the vector / distance of the point from origin
+     * @param angle The angle between the vector / point and the x-axis
+     */
+    public static fromPolar(length: number, angle: number): Vector2 {
+        let x = length * Math.cos(angle)
+        let y = length * Math.sin(angle)
+        return new Vector2(x, y)
+    }
+
+    public static from(other: TVector2): Vector2 {
+        return new Vector2(other.x, other.y)
+    }
 }
 
 /**
@@ -15,160 +167,4 @@ function clamp(value: number, min: number, max: number): number {
     return Math.max(min, Math.min(max, value));
 }
 
-/**
- * Return the (counter-clockwise) angle between the vector and the x-axis (in radians)
- *
- *       . a
- *      /
- *     /
- *    /
- *   O-------> x
- *
- * E.g. for point {x: 1, y: 1}, the angle is PI / 4 radians
- */
-function angle(vector: Vector2): number {
-    return Math.atan2(-vector.y, -vector.x) + Math.PI;
-}
-
-/**
- * Return a 2d vector representing the distance between b and a.
- *
- * @param a An object {x,y} which is subtracted from
- * @param b An object {x,y} which is subtracted
- */
-function subtract(a: Vector2, b: Vector2): Vector2 {
-    return {
-        x: a.x - b.x,
-        y: a.y - b.y
-    }
-}
-
-/**
- * Return the length-squared of a 2d vector.
- *
- * This is a convenience function that can be used to optimize some calculations
- * because it is cheaper to calculate than the length.
- *
- * @param a A vector
- */
-function lengthSquared(a: Vector2): number {
-    return a.x * a.x + a.y * a.y;
-}
-
-/**
- * Return the length of a 2d vector.
- *
- * @param a
- */
-function length(a: Vector2): number {
-    return Math.sqrt(lengthSquared(a))
-}
-
-/**
- * The dot product of two vectors
- *
- * @param a A vector
- * @param b Another vector
- */
-function dot(a: Vector2, b: Vector2): number {
-    return a.x * b.x + a.y * b.y;
-}
-
-/**
- * The determinant of two vectors
- *
- * @param a A vector
- * @param b Another vector
- */
-function det(a: Vector2, b: Vector2): number {
-    return a.x * b.y - b.x * a.y;
-}
-
-/**
- * Calculate the counter-clockwise angle (in radians) between two vectors.
- *
- * @param a A vector
- * @param b Another vector
- */
-function angleBetween(a: Vector2, b: Vector2): number {
-    if (a === b) {
-        return NaN;
-    }
-
-    const denominator = Math.sqrt(lengthSquared(a) * lengthSquared(b));
-    let c = dot(a, b) / denominator;
-
-    // Handle possible numerical problems
-    let theta = Math.acos(clamp(c, -1, 1));
-
-    const rotation = Math.sign(det(a, b))
-    if (rotation === -1) {
-        theta = Math.PI * 2 - theta
-    }
-
-    return theta
-}
-
-/**
- * Calculate the distance between two points
- *
- * @param a A 2d point
- * @param b Another 2d point
- */
-function distance(a: Vector2, b: Vector2): number {
-    return Math.sqrt(distanceSquared(a, b));
-}
-
-/**
- * Calculate the distance squared between two points
- *
- * This is a convenience function that can be used to optimize some calculations
- * because it is cheaper to calculate than the distance.
- *
- * @param point A point
- * @param other Another point
- */
-function distanceSquared(point: Vector2, other: Vector2) {
-    const x = point.x - other.x;
-    const y = point.y - other.y;
-    return x * x + y * y;
-}
-
-/**
- * Add two vectors together
- *
- * @param a A vector
- * @param b Another vector
- */
-function add(a: Vector2, b: Vector2): Vector2 {
-    return {
-        x: a.x + b.x,
-        y: a.y + b.y
-    }
-}
-
-/**
- * Create a new 2d point/vector
- *
- * @param x The x-coordinate
- * @param y The y-coordinate
- */
-function create(x: number, y: number): Vector2  {
-    return {x, y}
-}
-
-/**
- * Create a new 2d point/vector from polar coordinates
- *
- * @param length The length of the vector / distance of the point from origin
- * @param angle The angle between the vector / point and the x-axis
- */
-function fromPolar(length: number, angle: number): Vector2 {
-    let x = length * Math.cos(angle)
-    let y = length * Math.sin(angle)
-    return {x, y}
-}
-
-export {add, angle, angleBetween, clamp, create, det, distance, dot, distanceSquared, fromPolar, length, lengthSquared, subtract}
-
-export type {Vector2}
+export { Vector2 }
--- tests/point.test.ts
+++ tests/point.test.ts
@@ -1,5 +1,5 @@
 import {expect, test} from '@jest/globals';
-import {clamp, angle, angleBetween, length, lengthSquared, add, fromPolar, create} from "../src/point";
+import {clamp, Vector2} from "../src/point";
 
 test('Clamp less than min should equal min', () => {
     expect(clamp(-1, 2, 10)).toBe(2);
@@ -17,98 +17,109 @@ test('Clamp between min and max should equal the initial value', () => {
 
 
 test('The length squared for side 5 or 3,4,5 triangle is 25', () => {
-    let point = {x: 3, y: 4}
-    expect(lengthSquared(point)).toBeCloseTo(25, Number.EPSILON);
+    let point = new Vector2(3, 4)
+    expect(point.lengthSquared()).toBeCloseTo(25, Number.EPSILON);
 });
 
 test('The hypotenuse of a triangle with width 3, and height 4 is 5 ', () => {
-    let point = {x: 3, y: 4}
-    expect(length(point)).toBeCloseTo(5, Number.EPSILON);
+    let point = new Vector2(3, 4)
+    expect(point.length()).toBeCloseTo(5, Number.EPSILON);
 });
 
 test ("A small counter-clockwise angle ", () => {
-    let x = {x:1, y: 0}
-    let xy = {x:1, y: 1}
-
-    expect(angleBetween(x, xy)).toBeCloseTo(Math.PI / 4, Number.EPSILON)
+    let x = new Vector2(1,0)
+    let xy = new Vector2(1,1)
+    expect(x.angleBetween(xy)).toBeCloseTo(Math.PI / 4, Number.EPSILON)
 })
 
 test ("An anti-clockwise right angle should be equiv to 90 degrees", () => {
-    let x = {x:1, y: 0}
-    let y = {x:0, y: 1}
-    expect(angleBetween(x, y)).toBeCloseTo( Math.PI / 2, Number.EPSILON)
+    let x = new Vector2(1,0)
+    let y = new Vector2(0,1)
+    expect(x.angleBetween(y)).toBeCloseTo( Math.PI / 2, Number.EPSILON)
 })
 
 test ("A clockwise right angle should be equiv to 270 degrees", () => {
-    let x = {x:1, y: 0}
-    let y = {x:0, y: -1}
-    expect(angleBetween(x, y)).toBeCloseTo( 3 * Math.PI / 2, Number.EPSILON)
+    let x = new Vector2(1,0)
+    let y = new Vector2(0,-1)
+    expect(x.angleBetween(y)).toBeCloseTo( 3 * Math.PI / 2, Number.EPSILON)
 })
 
 
 test ("A large counter-clockwise angle should be equiv to  315 degrees", () => {
-    let x = {x:1, y: 0}
-    let xy = {x:1, y: -1}
-    expect(angleBetween(x, xy)).toBeCloseTo( 7 * Math.PI / 4, Number.EPSILON)
+    let x = new Vector2(1,0)
+    let xy = new Vector2(1,-1)
+    expect(x.angleBetween(xy)).toBeCloseTo( 7 * Math.PI / 4, Number.EPSILON)
 })
 
 test ("This small clockwise angle should be equiv to 45 degrees", () => {
-    let x = {x:1, y: 0}
-    let xy = {x:1, y: -1}
-    expect(angleBetween(xy, x)).toBeCloseTo( Math.PI / 4, Number.EPSILON)
+    let x = new Vector2(1,0)
+    let xy = new Vector2(1,-1)
+    expect(xy.angleBetween(x)).toBeCloseTo( Math.PI / 4, Number.EPSILON)
 })
 
 
 test ("Add two points", () => {
-    let a = {x:1, y: 2}
-    let b = {x:3, y: 4}
-    let c = {x:4, y: 6}
-    expect(add(a, b)).toStrictEqual( c)
+    let a = new Vector2(1,2)
+    let b = new Vector2(3,4)
+    let c = new Vector2(4, 6)
+    expect(a.add(b)).toStrictEqual( c)
 })
 
 
 test ("When y is zero and x is positive the angle should be zero", () => {
-    let x = {x:1, y: 0}
-    expect(angle(x)).toBeCloseTo(  0, Number.EPSILON)
+    let x = new Vector2(1,0)
+    expect(x.angle()).toBeCloseTo(  0, Number.EPSILON)
 })
 
 test ("When y is zero and x is negative the angle should PI radians", () => {
-    let x = {x:-1, y: 0}
-    expect(angle(x)).toBeCloseTo(  Math.PI, Number.EPSILON)
+    let x = new Vector2(-1,0)
+    expect(x.angle()).toBeCloseTo(  Math.PI, Number.EPSILON)
 })
 
 test ("A 45 degree angle", () => {
-    let xy = {x: 1, y: 1}
-    expect(angle(xy)).toBeCloseTo(Math.PI / 4, Number.EPSILON)
+    let xy = new Vector2( 1,1)
+    expect(xy.angle()).toBeCloseTo(Math.PI / 4, Number.EPSILON)
 })
 
 test ("A 135 degree angle", () => {
-    let xy = {x:-1, y: 1}
-    expect(angle(xy)).toBeCloseTo(  3 / 4 * Math.PI, Number.EPSILON)
+    let xy = new Vector2(-1,1)
+    expect(xy.angle()).toBeCloseTo(  3 / 4 * Math.PI, Number.EPSILON)
 })
 
 test ("A 225 degree angle", () => {
-    let xy = {x:-1, y: -1}
-    expect(angle(xy)).toBeCloseTo(  5 / 4 * Math.PI, Number.EPSILON)
+    let xy = new Vector2(-1,-1)
+    expect(xy.angle()).toBeCloseTo(  5 / 4 * Math.PI, Number.EPSILON)
 })
 
 test("Polar coordinates", () => {
-    let a = fromPolar(2, 0)
-    let a_expected = create(2, 0)
+    let a = Vector2.fromPolar(2, 0)
+    let a_expected = new Vector2(2, 0)
     expect(a).toEqual(a_expected)
 
-    let b = fromPolar(2, Math.PI / 2)
-    let b_expected = create(0, 2)
+    let b = Vector2.fromPolar(2, Math.PI / 2)
+    let b_expected = new Vector2(0, 2)
     expect(b.x).toBeCloseTo(b_expected.x, Number.EPSILON)
     expect(b.y).toEqual(b_expected.y)
 
-    let c = fromPolar(2, Math.PI / 4)
-    let c_expected = create(Math.sqrt(2), Math.sqrt(2))
+    let c = Vector2.fromPolar(2, Math.PI / 4)
+    let c_expected = new Vector2(Math.sqrt(2), Math.sqrt(2))
     expect(c.x).toBeCloseTo(c_expected.x, Number.EPSILON)
     expect(c.y).toBeCloseTo(c_expected.y, Number.EPSILON)
 
-    let d = fromPolar(2, 3 * Math.PI / 2)
-    let d_expected = create(0, -2)
+    let d = Vector2.fromPolar(2, 3 * Math.PI / 2)
+    let d_expected = new Vector2(0, -2)
     expect(d.x).toBeCloseTo(d_expected.x, Number.EPSILON)
     expect(d.y).toBeCloseTo(d_expected.y, Number.EPSILON)
+})
+
+test("Vector2 subtract method is part of prototype", () => {
+    // This test exists to confirm that class methods are attached to the object prototype.
+    // If they were attached to the Object itself, then the memory usage would be much greater
+    let a = new Vector2(1, 2);
+    let b = new Vector2(3, 4);
+    let aSubtract = a.subtract
+    let bSubtract = b.subtract
+    expect(aSubtract).toEqual(bSubtract)
+    let prototypeSubtract = Vector2.prototype.subtract
+    expect(aSubtract).toEqual(prototypeSubtract)
 })
